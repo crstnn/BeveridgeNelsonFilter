2978b0304571e5c09d4b32babd3ec764fb6fba44
crstnn <54381541+crstnn@users.noreply.github.com>
2022-03-07 01:01:37 +1100

    Updates

---
+++ precache-manifest.a457a40e30fd4badd699236017d136ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab0107b9c9a7561317bf",
+    "url": "/static/css/main.1b8e690f.chunk.css"
+  },
+  {
+    "revision": "ab0107b9c9a7561317bf",
+    "url": "/static/js/main.ab0107b9.chunk.js"
+  },
+  {
+    "revision": "17803a3b2760261e7246",
+    "url": "/static/js/1.17803a3b.chunk.js"
+  },
+  {
+    "revision": "bc4951043e8e318fb7d1",
+    "url": "/static/js/2.bc495104.chunk.js"
+  },
+  {
+    "revision": "2a12359ab8d2394c837c",
+    "url": "/static/js/runtime~main.2a12359a.js"
+  },
+  {
+    "revision": "3dbb1b2520205a3bc1e5b7af1935a0ae",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BeveridgeNelsonFilter/precache-manifest.faeb20124ac5d4c69dddbb68df17af92.js"
+  "/precache-manifest.a457a40e30fd4badd699236017d136ec.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/BeveridgeNelsonFilter/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
