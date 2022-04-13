cb5b40d2d7477b4a465c4f4fea445cd2db1555f2
crstnn <54381541+crstnn@users.noreply.github.com>
2022-04-13 17:40:24 +1000

    testing gunicorn runner

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,9 @@
 from gunicorn.http import message
 from main_api import app
-
-message.MAX_REQUEST_LINE = 2 ** 14
+from gunicorn.app.wsgiapp import run
 
 if __name__ == "__main__":
+    message.MAX_REQUEST_LINE = 2 ** 14
+    run()
     app.run()
+
