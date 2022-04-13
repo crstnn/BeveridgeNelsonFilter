6b2ba35b5620959f841e1f4c56a4af8b2d634b3c
crstnn <54381541+crstnn@users.noreply.github.com>
2022-04-13 16:20:50 +1000

    fixed slug misname

--- bn-filter-app/src/components/UserForm.js
+++ bn-filter-app/src/components/UserForm.js
@@ -127,7 +127,7 @@ export class UserForm extends Component {
             )
                 .reduce(statePairToParam, '?');
 
-        const finalURL = this.baseBackendURL + this.userSpecifiedDataSlug + paramStr
+        const finalURL = this.baseBackendURL + this.bnfUserSpecifiedDataSlug + paramStr
 
         console.log(finalURL)
 
