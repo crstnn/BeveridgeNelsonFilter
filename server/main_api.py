e42d75a80d6a593679324d397d18f2f26922c72b
crstnn <54381541+crstnn@users.noreply.github.com>
2022-07-02 13:03:00 +1000

    lower fixedDelta default and min value

--- server/main_api.py
+++ server/main_api.py
@@ -44,7 +44,7 @@ def get_bnf_params():
     delta_select = int(request.args.get("delta_select"))
 
     fixed_delta_arg = request.args.get("fixed_delta", None)
-    fixed_delta = float(fixed_delta_arg) if fixed_delta_arg is not None and delta_select == 0 else 0.05
+    fixed_delta = float(fixed_delta_arg) if fixed_delta_arg is not None and delta_select == 0 else 0.005
 
     ib = request.args.get("ib") == "true"
     demean = request.args.get("demean")
