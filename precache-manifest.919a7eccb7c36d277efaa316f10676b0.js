bf9918e7e6b0dce4728299ecb24dcc5185208bc6
crstnn <54381541+crstnn@users.noreply.github.com>
2022-07-11 09:19:21 +1000

    Updates

--- precache-manifest.919a7eccb7c36d277efaa316f10676b0.js
+++ precache-manifest.919a7eccb7c36d277efaa316f10676b0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "d873dd12d5adf43cfed7",
+    "revision": "699ee7f7bc503968f135",
     "url": "/static/css/main.5bc546fa.chunk.css"
   },
   {
-    "revision": "d873dd12d5adf43cfed7",
-    "url": "/static/js/main.97457e7e.chunk.js"
+    "revision": "699ee7f7bc503968f135",
+    "url": "/static/js/main.07b35b94.chunk.js"
   },
   {
     "revision": "a6e2e15a5b2cb5c4d0b4",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/3.cd47f4ef.chunk.js"
   },
   {
-    "revision": "60d797e0ff914ed42e389d6b27c4bd7c",
+    "revision": "fc038fca21fc369b3073921cd8be8c58",
     "url": "/index.html"
   }
 ];
