66c491644a11131cc918ef45f4401eb71d6a7ebc
crstnn <54381541+crstnn@users.noreply.github.com>
2022-07-15 18:39:05 +1000

    changed params for issue #25

--- bn-filter-app/src/components/ParametersForm.js
+++ bn-filter-app/src/components/ParametersForm.js
@@ -141,7 +141,7 @@ export class ParametersForm extends Component {
                             <FormControl variant="standard" sx={{minWidth: 170}}>
                                 <TextField
                                     label={values.deltaSelect === 0 ? "Fixed Delta" : "Minimum Delta"}
-                                    title={values.deltaSelect === 0 ? "Fixed delta for estimation" : "Minimum threshold start point for grid search"}
+                                    title={values.deltaSelect === 0 ? "Fixed delta for estimation" : "Minimum threshold start point for grid search (with grid increments of 0.0005)"}
                                     onChange={handleNumberFieldChange('delta')}
                                     defaultValue={values.delta}
                                     disabled={this.isDisabled['delta']()}
