89cfaaaba5800fbc3b342e11d5ec8dff0e4b5838
crstnn <54381541+crstnn@users.noreply.github.com>
2024-03-29 14:18:10 +1100

    chore: revert model dir

--- server/init.R
+++ server/init.R
@@ -3,4 +3,6 @@
 # Additional R package dependencies go here
 #
 # (Using base R without any imports)
+#
 # ensure R buildpack is placed before other server buildpacks
+# buildpack: https://github.com/virtualstaticvoid/heroku-buildpack-r
