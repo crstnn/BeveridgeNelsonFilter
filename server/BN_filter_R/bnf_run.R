4e3df49797c2bac2817795e5f8132ec33ba163d6
crstnn <54381541+crstnn@users.noreply.github.com>
2025-01-17 22:13:27 +1100

    style(R): lint code for consistent formatting

--- server/BN_filter_R/bnf_run.R
+++ server/BN_filter_R/bnf_run.R
@@ -1,5 +1,5 @@
 ####################################################################################################
-# Example using US data to illustrate how to use the various 'bnf' methods 
+# Example using US data to illustrate how to use the various 'bnf' methods
 ####################################################################################################
 
 
@@ -12,30 +12,40 @@ source("bnf_fcns.R")
 
 # Read in US centric data to use for the demonstration
 #usdata <- read.csv(file = 'us_data.csv', header = T, stringsAsFactors = F)
-usdata <- read.csv(file = 'USGDP_updated.csv', header = F, stringsAsFactors = F)
+usdata <-
+  read.csv(file = 'USGDP_updated.csv',
+           header = F,
+           stringsAsFactors = F)
 
 # Make the series GDP a 'ts' object
 # type 'help(ts)' in the R console for more information
 #gdp <- ts(data = usdata$GDPC1, end = c(2016, 2), frequency = 4)
-gdp <- ts(data = usdata, end = c(2023, 2), frequency = 4)
+gdp <- ts(data = usdata,
+          end = c(2023, 2),
+          frequency = 4)
 
 #Take logs and multiply by 100
-y <- transform_series(y = gdp, take_log = T, pcode = "p1") # same as: log(raw_y) * 100.0
+y <-
+  transform_series(y = gdp,
+                   take_log = T,
+                   pcode = "p1") # same as: log(raw_y) * 100.0
 
 print(y)
 
 # Example: Automatically determined delta and full sample mean demeaning method
 cat("Example: log US real GDP\n\n")
 
-bnfOutput <- bnf(as.vector(y), 
-                 delta_select = 2, 
-                 demean = "dm",
-                 iterative = 100, 
-                 dynamic_bands = T,
-                 adjust_bands = T,
-                 outliers = c(293, 294),
-                 window = 40,               
-			           ib = T)  
+bnfOutput <- bnf(
+  as.vector(y),
+  delta_select = 2,
+  demean = "dm",
+  iterative = 100,
+  dynamic_bands = T,
+  adjust_bands = T,
+  outliers = c(293, 294),
+  window = 40,
+  ib = T
+)
 # @delta_select: set to 0 if use fixed delta, 1 if max amp-to-noise, 2 (default) if min var(trend shocks)
 # @demean = "nd", "sm", "dm", or "pm", where "nd" = no drift, "sm" = sample mean, "dm" = dynamic demeaning, "pm" = structural breaks
 # @iterative: set to >1 for max number of iterations for iterative dynamic demeaning (e.g., 100 (default))
